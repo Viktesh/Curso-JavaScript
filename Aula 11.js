8111b00e5e13f7df09f689412f9cf87b938ef713
“vitesh” <kollet.advogados@gmail.com>
2023-07-03 22:14:52 -0300

    ex new

---
+++ Aula 11.js
@@ -0,0 +1,8 @@
+var vel = 59
+console.log(`A velocidade do seu carro é ${vel}KM/h`)
+if (vel > 60) {
+    console.log(`Você ultrapassou a velocidade permitida de ${vel} Km/h`)
+} else {
+    console.log(`Você está dentro da velocidade permitida`)
+}
+console.log(`Sempre dirija dentro da velocidade permitida`)
