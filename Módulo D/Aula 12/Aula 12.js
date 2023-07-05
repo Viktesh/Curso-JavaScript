bd58d950f83223dc5c0d9bbd2980b61f585548fe
“vitesh” <kollet.advogados@gmail.com>
2023-07-04 21:22:06 -0300

    New Excercice

---
+++ Módulo D/Aula 12/Aula 12.js
@@ -0,0 +1,8 @@
+var idade = 70
+if (idade < 18) {
+    console.log('Não Vota')
+} else if ( idade <18 || idade >65) {
+        console.log('Voto Opcional')
+    } else {
+        console.log('Voto Obrigatório')
+    }
